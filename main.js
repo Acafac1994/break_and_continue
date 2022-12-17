8430cbd7cc4a35641ade108900e9aaafcc3ea468
A.V.9.4 <120322748+Acafac1994@users.noreply.github.com>
2022-12-17 19:06:08 +0100

    break_and_continue

---
+++ main.js
@@ -0,0 +1,31 @@
+function main () {
+
+var zahlen = [
+    100,
+    250,
+    755,
+    488,
+    100,
+    485,
+    500,
+    5000,
+    750,
+
+]
+
+for (let i = 0; i < zahlen.length; i++){
+
+    if(zahlen[i] >= 600)
+    continue; 
+
+    console.log(zahlen[i])
+}
+}
+
+// Beispiel: Gib mir Alle daten über 600 von aber ignoriere allen unter 600
+
+/*
+Zeile 3-4: variable erstellt
+
+
+*/
